b543f06937abb09dfc2fde1f972b24e772d02fc6
mozzdepha <tim.moss@mac.com>
2015-02-11 21:51:53 +0000

    initial commit

---
+++ springboottest/src/main/java/com/mozz/springboottest/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.mozz.springboottest;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+    List<Customer> findByLastName(@Param("name") String lastName);
+}
